49b6102114158628f2524e9a1390ea0d6580d7b9
Oscar Perez Arruti <oscarperez.agg@gmail.com>
2023-12-15 10:21:37 +0100

    TestFile and gitignore

--- test.py
+++
@@ -1,5 +0,0 @@
-from src.modules.ALGORITHM_MODULE import *
-
-
-df = pd.read_pickle(f"/Users/oscarperezarruti/Documents/Documentos/Repositorios/trabajo-fin-grado-23/src/temp/AAPL.pkl")
-df.to_excel("/Users/oscarperezarruti/Documents/Documentos/Repositorios/trabajo-fin-grado-23/src/temp/AAPL.xlsx")
